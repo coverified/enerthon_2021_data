077bc82d47d0ab018d7fab9fff2554e554a08611
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2021-11-08 02:52:59 +0100

    add timeseries data

--- converter/convert.mjs
+++ converter/convert.mjs
@@ -1,37 +1,60 @@
+import fs from 'fs';
 import ExcelJS from 'exceljs';
 
 const filename = '../input/Testdata Enerthon 21.xlsx';
-const powerPlants = [];
+const data = [];
 
 const workbook = new ExcelJS.Workbook();
 await workbook.xlsx.readFile(filename);
 
-const sheetMasterData = workbook.getWorksheet('Master Data');
+const masterData = workbook.getWorksheet('Master Data');
 
 for (let i = 0; i < 18; i++) {
-    const col = sheetMasterData.getColumn(i + 2);
-
-    powerPlants.push({
+    const col = masterData.getColumn(i + 2);
+    const plantData = workbook.getWorksheet(`PlanungsdatenAnlage${i + 1}`);
+    const obj = {
         // original data
-        'Name': sheetMasterData.getCell(`${String.fromCharCode(64 + col.number)}1`).value,
-        'Klarname': sheetMasterData.getCell(`${String.fromCharCode(64 + col.number)}2`).value,
-        'Anschlussnetzbetreiber': sheetMasterData.getCell(`${String.fromCharCode(64 + col.number)}3`).value,
-        'Anschlussnetzbetreiber MP-ID': sheetMasterData.getCell(`${String.fromCharCode(64 + col.number)}4`).value,
-        'SR-ID': sheetMasterData.getCell(`${String.fromCharCode(64 + col.number)}5`).value,
-        'Energietraeger': sheetMasterData.getCell(`${String.fromCharCode(64 + col.number)}6`).value,
-        'Regelzone': sheetMasterData.getCell(`${String.fromCharCode(64 + col.number)}7`).value,
-        'Enthaltene TR (TR-ID)': sheetMasterData.getCell(`${String.fromCharCode(64 + col.number)}8`).value,
-        'MasterNr. TR': sheetMasterData.getCell(`${String.fromCharCode(64 + col.number)}9`).value,
-        'Klarname TR': sheetMasterData.getCell(`${String.fromCharCode(64 + col.number)}10`).value,
-        'TR-ID': sheetMasterData.getCell(`${String.fromCharCode(64 + col.number)}11`).value,
-        'Nettonennleistung (TRs)': sheetMasterData.getCell(`${String.fromCharCode(64 + col.number)}12`).value,
-        'Geokoordinaten TRs': sheetMasterData.getCell(`${String.fromCharCode(64 + col.number)}13`).value,
+        'Name': masterData.getCell(`${String.fromCharCode(64 + col.number)}1`).value,
+        'Klarname': masterData.getCell(`${String.fromCharCode(64 + col.number)}2`).value,
+        'Anschlussnetzbetreiber': masterData.getCell(`${String.fromCharCode(64 + col.number)}3`).value,
+        'Anschlussnetzbetreiber MP-ID': masterData.getCell(`${String.fromCharCode(64 + col.number)}4`).value,
+        'SR-ID': masterData.getCell(`${String.fromCharCode(64 + col.number)}5`).value,
+        'Energietraeger': masterData.getCell(`${String.fromCharCode(64 + col.number)}6`).value,
+        'Regelzone': masterData.getCell(`${String.fromCharCode(64 + col.number)}7`).value,
+        'Enthaltene TR (TR-ID)': masterData.getCell(`${String.fromCharCode(64 + col.number)}8`).value,
+        'MasterNr. TR': masterData.getCell(`${String.fromCharCode(64 + col.number)}9`).value,
+        'Klarname TR': masterData.getCell(`${String.fromCharCode(64 + col.number)}10`).value,
+        'TR-ID': masterData.getCell(`${String.fromCharCode(64 + col.number)}11`).value,
+        'Nettonennleistung (TRs)': masterData.getCell(`${String.fromCharCode(64 + col.number)}12`).value,
+        'Geokoordinaten TRs': masterData.getCell(`${String.fromCharCode(64 + col.number)}13`).value,
 
         // parsed data
-        'power': parseFloat(sheetMasterData.getCell(`${String.fromCharCode(64 + col.number)}12`).value.replace(' kW', '').replace('.', '').replace(',', '.')),
-        'lat': parseFloat(sheetMasterData.getCell(`${String.fromCharCode(64 + col.number)}13`).value.split(' ')[0].replace('BreiteNord=', '').replace(',', '.')),
-        'lng': parseFloat(sheetMasterData.getCell(`${String.fromCharCode(64 + col.number)}13`).value.split(' ')[1].replace('LaengeOst=', '').replace(',', '.')),
-    });
+        'maxPower': parseFloat(masterData.getCell(`${String.fromCharCode(64 + col.number)}12`).value.replace(' kW', '').replace('.', '').replace(',', '.')),
+        'lat': parseFloat(masterData.getCell(`${String.fromCharCode(64 + col.number)}13`).value.split(' ')[0].replace('BreiteNord=', '').replace(',', '.')),
+        'lng': parseFloat(masterData.getCell(`${String.fromCharCode(64 + col.number)}13`).value.split(' ')[1].replace('LaengeOst=', '').replace(',', '.')),
+    };
+
+    for (let x = 0; x < 5; x++) {
+        for (let y = 0; y < 96; y++) {
+            let cell = plantData.getCell(`${String.fromCharCode(64 + x + 2)}${y + 9}`);
+            let plannedPower = '0';
+
+            if (cell.value) {
+                plannedPower = `${cell.value}`;
+            }
+
+            data.push({
+                ...obj,
+                'begin': new Date(new Date('2021-06-01T22:00Z').getTime() + (y * 15 * 60 * 1000) + (x * 24 * 60 * 60 * 1000)).toISOString(),
+                'end': new Date(new Date('2021-06-01T22:00Z').getTime() + ((y + 1) * 15 * 60 * 1000) + (x * 24 * 60 * 60 * 1000)).toISOString(),
+                'plannedPower': parseFloat(plannedPower.replace(',', '.')) * 1000,
+            });
+        }
+    }
 }
 
-console.log(powerPlants);
+fs.writeFile('../converted/data.ndjson', data.map(JSON.stringify).join('\n'), err => {
+    if (err) {
+        console.error(err);
+    }
+});
